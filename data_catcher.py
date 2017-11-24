f87ca0e22163a0dd57e8a250e3fba3e067c0db28
Dawid Bodych <dawidbodych@MacBook-Air-Dawid.local>
2017-11-24 12:20:32 +0100

    Initial commit

---
+++ data_catcher.py
@@ -0,0 +1,25 @@
+import urllib.request, json
+import threading
+from turtle import *
+import turtle
+from tkinter import *
+
+def get_long_lat():
+	with urllib.request.urlopen("http://api.open-notify.org/iss-now.json") as url:
+		data = json.loads(url.read().decode())
+		print(data["iss_position"])
+
+def position_every_second():
+	threading.Timer(0.5, position_every_second).start()
+	get_long_lat()
+
+
+screen = Screen()
+
+screen_x, screen_y = screen.screensize()
+
+screen.bgpic("world.jpg")
+
+screen.bgpic()
+
+position_every_second()
