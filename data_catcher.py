48dd206cf661a5549191f9bf7612b9bf09a1856f
sirdigital <dawidbodych@gmail.com>
2017-11-28 01:24:23 +0100

    changed solution for the matplotlib one added draw_position() method

--- data_catcher.py
+++ data_catcher.py
@@ -1,25 +1,51 @@
 import urllib.request, json
 import threading
-from turtle import *
-import turtle
-from tkinter import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
 
 def get_long_lat():
+
+	'''
+	Load ISS position data
+	:return: longitude, latitude dictionary
+	'''
+
 	with urllib.request.urlopen("http://api.open-notify.org/iss-now.json") as url:
 		data = json.loads(url.read().decode())
-		print(data["iss_position"])
+		return(data["iss_position"])
+
+def draw_position():
+
+	'''
+	Transforms current position of ISS to pixel values and draws it on the map
+	'''
+
+	dict = get_long_lat()
+
+	lat_in_px = 69.0 + (float(dict["latitude"]) + 90) * 10.6
+	long_in_px = 39.0 + (float(dict["longitude"]) + 180) * 10.6
+
+	plt.scatter(x=long_in_px, y=lat_in_px, c="r", s=60)
+
+	plt.draw()
 
 def position_every_second():
+
+	'''
+	Starts a thread activating draw_position() method every 0.5 second
+	'''
+
 	threading.Timer(0.5, position_every_second).start()
-	get_long_lat()
+	draw_position()
 
+img = plt.imread("world.jpg")
+fig, ax = plt.subplots()
 
-screen = Screen()
+dict = get_long_lat()
 
-screen_x, screen_y = screen.screensize()
-
-screen.bgpic("world.jpg")
-
-screen.bgpic()
+plt.ion()
 
 position_every_second()
+
+plt.show(block=True)
